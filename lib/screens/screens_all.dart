a80ec409a17f7676b8acaff19c9221d4c4b41293
mrcodefrost <mr.codefrost@gmail.com>
2024-04-24 16:29:23 +0530

    deleted calls and notifs screen

--- lib/screens/screens_all.dart
+++ lib/screens/screens_all.dart
@@ -1,8 +1,6 @@
-export 'calls_screen.dart';
 export 'chat_screen.dart';
 export 'contacts_screen.dart';
 export 'home_screen.dart';
 export 'messages_screen.dart';
-export 'notifications_screen.dart';
 export 'profile_screen.dart';
 export 'select_user_screen.dart';
