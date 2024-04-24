eeced8a67de193cfd9c8c7cbc6901869afb6efe1
Daksh <72447688+mrcodefrost@users.noreply.github.com>
2024-04-24 23:38:45 +0530

    breach fix

--- lib/stream_keys.dart
+++ lib/stream_keys.dart
@@ -7,8 +7,6 @@ const apiKey = "unhmztx7m8jh";
 // token for id -
 const userToken = '';
 
-// secret : vttsm3mynungy5u4rsvfktwzazh823z7h6mqmyr7fdp6vpmbqtjk3f2x85eyx42k
-
 // Generate tokens from here
 // each token is unique to the user id, there is a generator on page that will require
 // secret key and user id to generate a token
